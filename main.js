76dee08815f4af728550f84bba47ac91b8163bfc
Jenine Marquez <jeninemarquez@Jenines-MacBook-Air.local>
2020-03-23 00:57:57 -0400

    Add water temp variable, time out function

--- main.js
+++ main.js
@@ -17,7 +17,36 @@ const printFreezing = () => {freezingTemp.innerText = "32"};
 printFreezing();
 
 
-//setTimeOut()
-setTimeout(function() {
+//Any number of your choice prints immediately inside of the span with the ID "water-temp"
 
-},3000);
+const waterTemp = document.getElementById('water-temp');
+
+const printWater = () => {waterTemp.innerText = "80"};
+
+printWater();
+
+//setTimeOut()
+
+const boiling = document.getElementById("boiling");
+const frozen = document.getElementById("frozen");
+const goodTemp = document.getElementById("good-temp");
+
+
+function waterMessage(){
+    setTimeout( () => {
+        if (waterTemp > 212) {
+            boiling.classList.remove('hide');
+            boiling.classList.add('show');
+        }
+        else if (waterTemp < 32) {
+            frozen.classList.remove('hide');
+            frozen.classList.add('show');
+        }
+        else {
+            goodTemp.classList.remove("hide");
+            goodTemp.classList.add("show");
+        }
+    },3000);
+} 
+
+waterMessage();
