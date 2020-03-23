6e1ef1a801485da70ca86c9a4fd83849f5c652be
Jenine Marquez <jeninemarquez@Jenines-MacBook-Air.local>
2020-03-23 00:08:57 -0400

    Write arrow functions for boiling and freezing temps

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 // It is always helpful to use comments in your code!
+
+//Print "212" immediately inside the span with the ID "boiling-temp" using an arrow function
+const boilingTemp = document.getElementById('boiling-temp');
+
+const printBoiling = () => {boilingTemp.innerText = "212"};
+
+printBoiling();
+
+
+//Print "32" immediately inside the span with the ID "freezing-temp" using an arrow function
+
+const freezingTemp = document.getElementById('freezing-temp');
+
+const printFreezing = () => {freezingTemp.innerText = "32"};
+
+printFreezing();
+
+
+//setTimeOut()
+setTimeout(function() {
+
+},3000);
